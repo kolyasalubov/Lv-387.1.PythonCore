c83b52739e3e29a486def12061577878b36dd109
Yuriy Abragamets <yuri.trade17@gmail.com>
2019-02-24 20:48:42 +0200

    add file lesson1

---
+++ lesson1.py
@@ -0,0 +1,32 @@
+var_int = 10
+var_float = 8.4
+var_str = "No"
+
+big_int = var_int * 3.5
+var_float = var_float - 1
+print("var_int / var_float =", var_int / var_float)
+print("big_int / var_float=", big_int / var_float)
+
+var_str2 = "Yes"
+
+vrar_str = 2*var_str+3*var_str2
+print("var_int=",var_int,
+      "big_int=", big_int,
+      "var_float=", var_float,
+      "var_str=", var_str)
+
+
+name = input("Whats Your name?")
+age = input("How old are U?")
+city = str(input("Where do U live?"))
+print("Hello,", name,"!")
+print("Your age is:", age)
+print("You live in ", city)
+
+
+
+a=int(input("Input value A: "))
+b=int(input("Input value B: "))
+print("a+b=", a+b)
+print("a-b=", a-b)
+
