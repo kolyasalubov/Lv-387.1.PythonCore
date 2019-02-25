f60c8ec3e67b99db88be70d57d2798e0611a5c31
budzovych <maria.budzovych@gmail.com>
2019-02-25 14:14:41 +0200

    add new file task1.py

---
+++ task1.py
@@ -0,0 +1,27 @@
+#task 1
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+residence = input("Where do you live?: ")
+print("Hello " + name)
+print("Your age is " + age)
+print("You live in " + residence)
+
+#task2
+a = input ("Input a: ")
+b = input ("Input b: ")
+print("a + b = ", int(a) + int(b))
+print("a - b = ", int(a) - int(b))
+
+#task3
+var_int = 10
+var_float = 8.4
+var_str = "No"
+big_int = var_int * 3.5
+var_float = var_float - 1 
+print (var_int / var_float)
+print (big_int / var_float)
+var_str1 = var_str*2 + "Yes"*3
+print(var_int)
+print(big_int)
+print(var_float)
+print(var_str1)
